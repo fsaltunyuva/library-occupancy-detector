ac81bc503a52304c6cd85218d0ae69eee6c083b8
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2024-05-21 17:11:36 +0300

    Confidence Level Text on Box

--- YOLO-Experimental-V1/yolo-webcam.py
+++ YOLO-Experimental-V1/yolo-webcam.py
@@ -1,15 +1,16 @@
 from ultralytics import YOLO
 import cv2
 import math
-# start webcam
+
+# Start webcam
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
-# model
+# Load model
 model = YOLO("yolo-Weights/yolov8n.pt")
 
-# object classes
+# Object classes
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
               "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
@@ -26,37 +27,42 @@ while True:
     success, img = cap.read()
     results = model(img, stream=True)
 
-    # coordinates
+    # Coordinates
     for r in results:
         boxes = r.boxes
 
         for box in boxes:
-            # bounding box
+            # Bounding box
             x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # Convert to int values
 
-            # put box in cam
+            # Draw bounding box
             cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
-            # confidence
+            # Confidence
             confidence = math.ceil((box.conf[0]*100))/100
-            print("Confidence --->",confidence)
+            print("Confidence --->", confidence)
 
-            if confidence < 0.8:
-                continue
-
-            # class name
+            # Class name
             cls = int(box.cls[0])
-            #print("Class name -->", classNames[cls])
+            # print("Class name -->", classNames[cls])
 
-            # object details
+            # Add confidence to the top right corner of the box
+            text = f"{confidence}"
+            text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.9, 2)
+            text_x = x2 - text_size[0]
+            text_y = y1 - 10 if y1 - 10 > 10 else y1 + 10  # Adjust text position if it's too close to the top
+
+            cv2.putText(img, text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+
+            # Object details (optional)
             org = [x1, y1]
             font = cv2.FONT_HERSHEY_SIMPLEX
             fontScale = 1
             color = (255, 0, 0)
             thickness = 2
 
-            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+            # cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
