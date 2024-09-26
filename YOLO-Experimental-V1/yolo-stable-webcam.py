b9dac13c5f1af3ee4b4617ea0089336e644178cf
Furkan Safa Altunyuva <103141929+fsaltunyuva@users.noreply.github.com>
2024-09-26 12:29:18 +0300

    Refactoring YOLOv10 Upgrade

--- YOLO-Experimental-V1/yolo-stable-webcam.py
+++ YOLO-Experimental-V1/yolo-stable-webcam.py
@@ -12,7 +12,8 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 # Load model
-model = YOLO("yolo-Weights/yolov10n.pt")
+# model = YOLO("yolo-Weights/yolov10n.pt")
+model = YOLO("yolov10n.pt")
 
 # Object classes
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
